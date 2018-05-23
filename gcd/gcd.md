effc5f96aa04eac3c2b778f48af0fce2465001da
Tim Siwula <tim.siwula@consensys.net>
2018-05-22 23:57:23 -0500

    created simole gcd package that prints out static results

---
+++ gcd/gcd.md
@@ -0,0 +1,18 @@
+# A gcd function
+
+
+## build and run the program
+``` 
+cargo run
+```
+
+## clean up generated files
+``` 
+cargo clean
+```
+
+
+
+
+
+
---
+++ gcd/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("running gcd(5, 100)");
+    let result: u64 = gcd(5, 100);
+    println!("result = {}", result);
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+
+        m = m % n;
+    }
+
+    n
+}
